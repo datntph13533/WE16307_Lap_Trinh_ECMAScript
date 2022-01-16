a3d9ea66d08cbad5bc208a8408f200d5bd08d724
datntph13533 <datntph13533@fpt.edu.vn>
2022-01-16 10:43:28 +0700

    update

--- src/main.js
+++ src/main.js
@@ -1,16 +1,34 @@
 import "../style.css";
 
 import Navigo from "navigo";
+import HomePage from "./pages/home";
+import Header from "./components/header";
+import ProductPage from "./pages/product";
+import AboutPage from "./pages/about";
+import Footer from "./components/footer";
+import DetailNewsPage from "./pages/DetailNewsPage";
 
 const router = new Navigo("/", { linksSelector: "a" });
+
+const print = (content) => {
+    document.getElementById("header").innerHTML = Header.render();
+    document.getElementById("app").innerHTML = content;
+    document.getElementById("footer").innerHTML = Footer.render();
+};
 
 router.on({
     "/": () => {
         print(HomePage.render());
     },
+    "/about": () => {
+        print(AboutPage.render());
+    },
+    "/product": () => {
+        print(ProductPage.render());
+    },
     "/news/:id": ({ data }) => {
         const { id } = data;
-        print(NewPage.render(id));
+        print(DetailNewsPage.render(id));
     },
 });
 router.resolve();
